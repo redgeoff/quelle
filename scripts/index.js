6a411496328c43e757d035ebce2b20a53024d5c8
Geoff Cox <redgeoff@gmail.com>
2017-07-11 07:59:55 -0700

    chore(version): 0.0.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var FilteredStreamIterator = require('./filtered-stream-iterator'),
+  PersistentStreamIterator = require('./persistent-stream-iterator'),
   PersistentStream = require('./persistent-stream'),
   StreamIterator = require('./stream-iterator');
 
 module.exports = {
   FilteredStreamIterator: FilteredStreamIterator,
+  PersistentStreamIterator: PersistentStreamIterator,
   PersistentStream: PersistentStream,
   StreamIterator: StreamIterator
 };
